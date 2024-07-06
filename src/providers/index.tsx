37d97fbe5c29ee5b0a41319402c9b63a78857062
shouta0715 <Myheroes0715@gmail.com>
2024-07-06 20:32:20 +0900

    add: auth provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,22 @@
-import { QueryClientProvider } from "@tanstack/react-query";
+import liff from "@line/liff";
+import { QueryClientProvider, useSuspenseQuery } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { getInitialClientState } from "@/lib/client";
+
+const liffId = import.meta.env.VITE_LIFF_ID;
+
+const getLiff = async () => {
+  await liff.init({ liffId });
+  const isLoggedIn = liff.isLoggedIn();
+
+  if (!isLoggedIn) {
+    liff.login();
+  }
+
+  const user = await liff.getProfile();
+
+  return { isLoggedIn, user };
+};
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
@@ -10,3 +26,16 @@ export function Provider({ children }: { children: React.ReactNode }) {
     </QueryClientProvider>
   );
 }
+
+export const LineAuthProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  useSuspenseQuery({
+    queryFn: getLiff,
+    queryKey: ["liff"],
+  });
+
+  return children;
+};
